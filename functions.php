e40c41e8239959d1c503522151b062f71f1d40bc
Bart Nagel <bart@tremby.net>
2011-08-04 15:30:22 +0100

    added substitute for sys_get_temp_dir for php <5.2.1

--- functions.php
+++ functions.php
@@ -99,4 +99,21 @@ if (!function_exists("json_encode")) {
 	}
 }
 
+if (!function_exists("sys_get_temp_dir")) {
+	function sys_get_temp_dir() {
+		if ($temp = getenv("TMP"))
+			return $temp;
+		if ($temp = getenv("TEMP"))
+			return $temp;
+		if ($temp = getenv("TMPDIR"))
+			return $temp;
+		$temp = tempnam(__FILE__, "");
+		if (file_exists($temp)) {
+			unlink($temp);
+			return dirname($temp);
+		}
+		return null;
+	}
+}
+
 ?>
