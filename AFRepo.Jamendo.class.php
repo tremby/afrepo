741e64cf3f6b61349c6d03c542ed3fe30097ac95
Bart Nagel <bart@tremby.net>
2011-08-17 10:39:14 +0100

    cache jamendo file list since generating it is expensive

--- AFRepo.Jamendo.class.php
+++ AFRepo.Jamendo.class.php
@@ -21,13 +21,24 @@ class AFRepo extends AFRepoBase {
 		if (!is_null($this->allfiles))
 			return $this->allfiles;
 
-		$this->allfiles = array();
+		// if cache file exists use it
+		$cachefile = "/tmp/jamendo_filelist_cache";
+		if (file_exists($cachefile))
+			return $this->allfiles = unserialize(file_get_contents($cachefile));
+
+		trigger_error("generating all files list", E_USER_NOTICE);
+
+		// get list of files in dir
 		$path = dirname(__FILE__) . "/jamendo";
 		$dir = dir($path);
+		$this->allfiles = array();
 		while (($file = $dir->read()) !== false)
 			if ($file[0] != ".")
 				$this->allfiles[realpath($path . "/" . $file)] = true;
 		$dir->close();
+
+		// write cache file
+		file_put_contents($cachefile, serialize($this->allfiles));
 
 		return $this->allfiles;
 	}
